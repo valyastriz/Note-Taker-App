b9d73828eee253b587c0571d358c4baaa2fa0e28
valyastriz <valyastriz@gmail.com>
2024-07-17 10:29:07 -0400

    added logic to server.js to render index.html and notes.html files

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+// const api = require('./routes/index.js')
 
-app.get('/', (req, res) => {
-    res.send('TEST');
-});
+
+//GET Route for notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+//GET Route for index.html
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 
 const PORT = process.env.PORT || 3002;
